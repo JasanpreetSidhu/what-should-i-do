d5c2657b96774acb33fd62361ff39cf0e5678c34
Jasanpreet Singh Sidhu <jasanpreetsinghsidhu@gmail.com>
2025-09-16 21:23:38 +0000

    html page and button function in js file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function getRandomTask() {
+  tasks = [];
+  
+  if (tasks.length == 0) {
+    return "No tasks in the list"
+  } else {
+    // Math.random() * num => ( 0 to 0.99999... ) * num = 0 to (num-1).99999...
+    randIndex =  parseInt( Math.random() * tasks.length );
+    return tasks[randIndex];
+  }
+
+}
