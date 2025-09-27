97c576e7615b0270eb209f5ffca90d7db29a3d21
jasanpreet-sidhu <jasanpreetsinghsidhu@gmail.com>
2025-09-26 22:48:38 -0400

    fetching from backend server

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ function getRandomTask() {
 		'Practice on NoSQL databases',
 		'Practice leetcode problems',
 		'create full-stack project using linkedin learning',
-    'check google trending articles',
-    'read about recent updates in software industry',
+		'check google trending articles',
+		'read about recent updates in software industry',
 	];
 
 	if (tasks.length == 0) {
@@ -21,21 +21,23 @@ function getRandomTask() {
 	}
 }
 
-function renderRandomTask() {
-	const randTask = getRandomTask();
+async function renderRandomTask() {
+	const randTask = await fetch(
+		'https://random-task-selector-backend.vercel.app/'
+	);
 
-  let randTaskNode = document.getElementById('task-shown');
+	let randTaskNode = document.getElementById('task-shown');
 
-  if (randTaskNode) {
-    randTaskNode.innerText = randTask;
-  } else {
-    const header2 = document.createElement('h2');
-    header2.id = 'task-shown';
-    randTaskNode = document.createTextNode(randTask);
-    header2.appendChild(randTaskNode);
-  
-    const diceImgNode = document.querySelector('section img');
-  
-    document.getElementById('hero').replaceChild(header2, diceImgNode);
-  }
+	if (randTaskNode) {
+		randTaskNode.innerText = randTask;
+	} else {
+		const header2 = document.createElement('h2');
+		header2.id = 'task-shown';
+		randTaskNode = document.createTextNode(randTask);
+		header2.appendChild(randTaskNode);
+
+		const diceImgNode = document.querySelector('section img');
+
+		document.getElementById('hero').replaceChild(header2, diceImgNode);
+	}
 }
